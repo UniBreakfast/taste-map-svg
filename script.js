5e4f26a9522faac8c3ebfd69459980419173c864
UniBreakfast <unibreakfast1@gmail.com>
2023-06-04 17:15:20 +0300

    add labels

--- script.js
+++ script.js
@@ -99,27 +99,30 @@ const cats = [
 const design = {
   r: 175,
   count: 5,
-  color1: '#C0E0B4', 
-  color2: '#ABD296', 
+  color1: '#C0E0B4',
+  color2: '#ABD296',
   gap: 35,
 
 }
+const cx = 100
+const cy = 65
 const size = design.r * 2 + design.gap * 2
 const ratio = size / 100
 const { PI, sin, cos } = Math
 const svg = document.querySelector('svg')
 const polygon = document.querySelector('polygon')
+const labelGroup = document.querySelector('g')
 
 
 renderTasteMap(cats, design)
 
 function renderTasteMap(cats, design) {
   const { r, count, color1, color2, gap } = design
+  const points = drawAxis(r + gap, cats.length)
 
-  drawAxis(r + gap, cats.length)
   drawCircles(gap, count, color1, color2)
   drawStar(cats, r, gap)
-  addLabels(cats, r, gap)
+  addLabels(points, cats, r, gap)
 }
 
 function drawStar(cats, r, gap) {
@@ -162,22 +165,28 @@ function drawPath(points) {
 }
 
 function drawAxis(r, count) {
+  const points = []
+
   for (let i = 0; i < count; i++) {
     const line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
     const angle = 2 * PI * (i / count) - PI / 2
     const x = r * cos(angle)
     const y = r * sin(angle)
+    const point = convert([x, y])
 
-    line.setAttribute('x1', 50)
-    line.setAttribute('y1', 50)
-    line.setAttribute('x2', x / ratio + 50)
-    line.setAttribute('y2', y / ratio + 50)
+    line.setAttribute('x1', cx)
+    line.setAttribute('y1', cy)
+    line.setAttribute('x2', point[0])
+    line.setAttribute('y2', point[1])
     line.setAttribute('stroke', '#000000')
     line.setAttribute('stroke-width', 0.1)
     line.setAttribute('stroke-dasharray', '2 1')
 
     svg.prepend(line)
+    points.push(point)
   }
+
+  return points
 }
 
 function drawCircles(r, count, color1, color2) {
@@ -185,23 +194,54 @@ function drawCircles(r, count, color1, color2) {
     const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
     const color = i % 2 ? color1 : color2
 
-    circle.setAttribute('cx', 50)
-    circle.setAttribute('cy', 50)
-    circle.setAttribute('r', r/ratio * (i + 1))
+    circle.setAttribute('cx', cx)
+    circle.setAttribute('cy', cy)
+    circle.setAttribute('r', r / ratio * (i + 1))
     circle.setAttribute('fill', color)
 
     svg.prepend(circle)
-    
+
     if (i === count - 1) {
       circle.setAttribute('filter', 'url(#shadow)')
     }
   }
 }
 
-function addLabels(cats, r, gap) {
+function addLabels(points, cats, r, gap) {
+  for (let i = 0; i < cats.length; i++) {
+    const text = document.createElementNS('http://www.w3.org/2000/svg', 'text')
+    const angle = 2 * PI * (i / cats.length) - PI / 2
+    const x = (r + gap) * cos(angle)
+    const y = (r + gap) * sin(angle)
+    const point = convert([x, y])
+
+    text.setAttribute('x', point[0] + 2)
+    text.setAttribute('y', point[1])
+    text.setAttribute('dominant-baseline', 'middle')
+    text.setAttribute('font-size', '4')
+    text.setAttribute('font-weight', 'bold')
+    text.textContent = cats[i].name
+
+    if (i > cats.length / 2) {
+      text.setAttribute('text-anchor', 'end')
+      text.setAttribute('x', point[0] - 2)
+    } else if (!i) {
+      text.setAttribute('x', point[0])
+      text.setAttribute('y', point[1] - 2)
+      text.setAttribute('text-anchor', 'middle')
+      text.setAttribute('dominant-baseline', 'auto')
+    } else if (i == cats.length / 2) {
+      text.setAttribute('x', point[0])
+      text.setAttribute('y', point[1] + 1)
+      text.setAttribute('text-anchor', 'middle')
+      text.setAttribute('dominant-baseline', 'hanging')
+    }
+
+    labelGroup.prepend(text)
+  }
 
 }
 
 function convert([x, y]) {
-  return [x / ratio + 50, y / ratio + 50]
+  return [x / ratio + cx, y / ratio + cy]
 }
